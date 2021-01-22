c39dcff434df14819240b02d6495f667702b91bf
unknown <mo.metwaly94@gmail.com>
2021-01-22 15:08:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter' 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Counter />
+      {/*<Greet  name="Mohamed"/>
+      <Greet  name="Ahmed"/>
+      <Greet  name="Metwaly"/>
+      <Welcome name="Mohamed"/>
+      <Welcome name="Ahmed"/>
+      <Welcome name="Metwaly"/>
+      <Hello />
+      <Message />*/}
+  </div>
   );
 }
 
